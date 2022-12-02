f72d0fd4d25c7610fc6d70354907615019c1a5fb
Kalvium-Program <109285740+Kalvium-Program@users.noreply.github.com>
2022-12-02 15:23:24 +0530

    3 files modified

--- tests/FunctionsTest.js
+++ tests/FunctionsTest.js
@@ -1,47 +1,29 @@
-describe("Closures and Scope - ",function(){
-
-  const arrayOfNumbers = [1,2,3,4,5,6];
+describe('Closures and Scope - ', function () {
+  const arrayOfNumbers = [1, 2, 3, 4, 5, 6];
   const notPresentItem = 9;
   const presentItem = 4;
 
-  var createPopAttributes;
-  beforeEach(function() {
-    createPopAttributes = {
-      currIndex: -1000000,
-      check: false,    
-    };
+  it('Define a function: createPop()', function () {
+    expect(typeof createPop).toBe('function');
   });
 
-  it("Define a function: createPop()", function(){
-    expect(typeof createPop).toBe("function");
-  });
-
-  it("Initialize the attributes with values as -> currIndex:-1000000, check:false", function() {
-    expect(createPopAttributes).toEqual(jasmine.objectContaining({
-      currIndex:-1000000,
-      check: false
-    }));
-  });
-
-  it("Return `The item is not present and is at index -1000000`, when item is not present in the given array", function(){
-    const dummy = createPop(arrayOfNumbers,notPresentItem);
+  it('Return `The item is not present and is at index -1000000`, when item is not present in the given array', function () {
+    const dummy = createPop(arrayOfNumbers, notPresentItem);
 
     actualResult = `The item is not present and is at index -1000000`;
 
-    expectedResult = dummy()
+    expectedResult = dummy();
 
     expect(actualResult).toEqual(expectedResult);
-  })
+  });
 
-  it("Return `The item is present and is at index {the index of that item}`, when item is present in the given array", function(){
-    const dummy = createPop(arrayOfNumbers,presentItem);
+  it('Return `The item is present and is at index {the index of that item}`, when item is present in the given array', function () {
+    const dummy = createPop(arrayOfNumbers, presentItem);
 
     actualResult = `The item is present and is at index 3`;
 
-    expectedResult = dummy()
-    
-    expect(actualResult).toEqual(expectedResult);
-  })
-  
-});
+    expectedResult = dummy();
 
+    expect(actualResult).toEqual(expectedResult);
+  });
+});
